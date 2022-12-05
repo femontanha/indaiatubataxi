ea980986eb70fefbe9099bd53b93d5c5c3fa945f
Fellipe Azambuja <femontanhanew@gmail.com>
2022-12-05 20:48:38 -0300

    Add nprogress to pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,38 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 import WhatsApp from '../components/WhatsApp'
 import Header from '../components/Header'
 import Container from '../components/Container'
 import Footer from '../components/Footer'
+import 'nprogress/nprogress.css'
+
+const NProgress = require('nprogress')
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleStart = (url: string) => {
+      NProgress.start()
+    }
+
+    const handleStop = () => {
+      NProgress.done()
+    }
+
+    router.events.on('routeChangeStart', handleStart)
+    router.events.on('routeChangeComplete', handleStop)
+    router.events.on('routeChangeError', handleStop)
+
+    return () => {
+      router.events.off('routeChangeStart', handleStart)
+      router.events.off('routeChangeComplete', handleStop)
+      router.events.off('routeChangeError', handleStop)
+    }
+  }, [router])
+
   return (
     <>
       <WhatsApp />
