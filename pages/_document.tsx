e9adebfaf56ef4238562726151d6db6ebe05d8bc
Fellipe Azambuja <femontanhanew@gmail.com>
2022-12-05 21:13:16 -0300

    Update webmanifest

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -32,7 +32,7 @@ export default function Document() {
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="manifest" href="/manifest.json" />
+        <link rel="manifest" href="/site.webmanifest"></link>
         <meta name="msapplication-TileColor" content="#ffd203" />
         <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
         <meta name="theme-color" content="#ffd203"></meta>
